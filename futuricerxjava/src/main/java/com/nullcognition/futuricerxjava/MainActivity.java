00b403299e1e030f69ceb56706aa002dc9036b27
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-09-25 10:44:39 -0400

    added: futurice article tips all 7

--- futuricerxjava/src/main/java/com/nullcognition/futuricerxjava/MainActivity.java
+++ futuricerxjava/src/main/java/com/nullcognition/futuricerxjava/MainActivity.java
@@ -62,6 +62,64 @@ public class MainActivity extends AppCompatActivity{
 		// PublishSubject is has multiple subscribers, to which it broadcasts onNext events, and terminates
 		// the connection when onError, or onCompletion is called
 
+		// others: behaviour subject, async subject, publish subject, replay subject
+	}
+
+	private void tip04(){
+
+		// async operations that go out of the ui thread must often come back to it
+		// change threads that are operating on with subscribeOn(...), thus making the call async
+		// to go back use observeOn(AndroidSchedulers.mainThread())
+
+		// do switch to main thread in data layer, right after the operation has completed
+		// if the value is coming synchronously from the cache, the switch in the main thread
+		// can cause rendering delay
+
+		// custom Observable cannot be unsubscribed from until its onSubscribe function has finished
+		// thus any blocking calls on the thread will keep the observable from being unsubbed
+	}
+
+	private void tip05(){
+
+		/* read the rxjava docs - main ones being
+
+		Observable.from() - synchronously converts iterable to observable, emitting all values then calling onComplete
+
+		Observable.just(<Object>...) - like ^ but emits one object on onNext then onComplete
+
+		Observable.create - create your own Observable if all logic fits within the single closure
+
+		Observable.merge - combine 2+ observables
+
+		Observable.error - emits an error
+
+		.map - all values of a stream are mapped(operated on) and the products is another stream
+
+		.filter - takes a function that evaluates to true or false, thus filtering the streams value based on the result
+
+		.combineLatest - take two latest values, combined to a new one, triggers even if one value changes
+
+		.zip - like ^ but both values must be new
+
+		.manyMap/.flatMap - chaining async returns, appending one stream to another
+
+		.cache - like onSubscribe, but provides the cached value - to all or just the subscriber?
+		*/
+	}
+
+	private void tip06(){
+		// subscribing with Observer(has onNext,onCompleted, onError) vs Action1(can override only one call per sub, create another action for 2/3 else use observable)
+	}
+
+	private void tip07(){
+		// subscriptions leak memory, by subbing, you give a strong reference to the observer(a closure/anon inner class)
+		// thus the observer will not be gc'd untill the Observable is.
+		// call subscribe to return the subscription, then unsub, knowing when is difficult
+		// create a custom WeakReference scheme in your application, or to make sure everything is properly unsubscribed from
+
+		// ave activities and fragments handle all subscriptions to static instances and unsubscribe accordingly. This would include not letting views make
+		// subscriptions to static instances at all. For this approach you can also check the AndroidObservable operation in the RxJava to tie the observable to an
+		// activity or a fragment. It, for instance, deactivates the observable if the fragment is not added
 	}
 
 	public static class Subber extends Subscriber<Integer>{
@@ -71,3 +129,38 @@ public class MainActivity extends AppCompatActivity{
 		@Override public void onNext(final Integer integer){ Log.d("logErr", Integer.toString(integer));}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
